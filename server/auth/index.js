49b17302c480922ad4c10bf46a4bfc84db9e81f0
jeru <mikurej95@gmail.com>
2018-11-19 00:59:45 -0600

    started login api route. need to add passport middleware with sessions

--- server/auth/index.js
+++ server/auth/index.js
@@ -4,6 +4,27 @@ module.exports = router;
 
 router.post('/login', async (req, res, next) => {
   try {
+    const { login, password } = req.body;
+
+    const loginType = login.includes('@') ? 'email' : 'username';
+
+    const user = await User.findOne({
+      where: { [loginType]: login },
+    });
+
+    if (!user) {
+      console.log('No such user found:', login);
+      res
+        .status(401)
+        .send(
+          `The username you entered doesn't appear to belong to an account. Please check your username and try again.`
+        );
+    } else if (!user.correctPassword(password)) {
+      console.log('Incorrect password for user:', login);
+      res.status(401).send('Incorrect password');
+    } else {
+      req.login();
+    }
   } catch (err) {
     next(err);
   }
