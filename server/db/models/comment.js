d1c05d8e7f1de018844c4b24c37b53c22f7a907e
jeru <mikurej95@gmail.com>
2018-12-03 07:53:56 -0600

    defined models and associations

---
+++ server/db/models/comment.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Comment = db.define('comment', {
+  text: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmtpy: true,
+    },
+  },
+});
+
+module.exports = Comment;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,31 @@
 const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+const Like = require('./like');
+const Relationship = require('./relationship');
+const Tag = require('./tag');
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+User.hasMany(Comment);
+Comment.belongsTo(Post);
+
+User.hasMany(Like);
+Like.belongsTo(User);
+Comment.hasMany(Like);
+Like.belongsTo(Comment);
+
+// relationship
+
+Post.hasMany(Tag);
+Tag.belongsToMany(Post);
 
 module.exports = {
   User,
+  Post,
+  Comment,
+  Like,
+  Relationship,
+  Tag,
 };
