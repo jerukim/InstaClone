774fea09cf01640cc2ce3696e2cbfc52d796f6a1
jeru <mikurej95@gmail.com>
2019-01-31 08:06:34 -0600

    updated sequelize arguments

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,16 @@ const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
+  'instaclone',
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
   {
-    logging: false,
+    host: 'Instaclone-env.cnnhnf83mp.us-east-2.elasticbeanstalk.com',
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: 'Amazon RDS',
+    },
   }
 );
 
