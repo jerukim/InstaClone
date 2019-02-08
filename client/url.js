f8986e2fc84f2e0f29317172da5ac2766828beb1
jeru <mikurej95@gmail.com>
2019-02-08 05:33:11 -0600

    changed baseURL to change based on dev/prod env

--- client/url.js
+++ client/url.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-export const baseURL =
-  'Instaclone-env-1.cnnhnf83mp.us-east-2.elasticbeanstalk.com';
+export const baseURL = process.env.NODE_ENV === 'development' ? process.env.DEV_URL : process.env.URL;
 
 const ax = axios.create({
   baseURL,
